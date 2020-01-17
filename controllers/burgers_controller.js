cff004738dcb5bd395da2e60280ca04b2e9002f2
Clarisse Moraes <clarissem95@live.com>
2020-01-16 20:43:30 -0500

    one last commit before adding handlebars code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ var hbsObject = {
     });
 });
 // POST/CREATE BURGER
-router.post('/api/burgers', function(req, res) {
+router.post('/api/burger', function(req, res) {
 burger.create(['burger_name', 'devoured'], 
 [
     req.body.burger_name,
@@ -50,7 +50,7 @@ condition, function(result) {
 router.delete("/api/burgers/:id", function(req, res) {
 var condition = "id = " + req.params.id;
 
-cat.delete(condition, function(result) {
+burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
     // If no rows were changed, then the ID must not exist, so 404
     return res.status(404).end();
